c32c6d215f84d669bb49e02c1c6ed693ae1e7bf2
Denis Yaremov <lu4@Deniss-MBP.technicolor.net>
2022-12-18 22:11:19 +0100

    Added references

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// Based on:
+// https://vector-of-bool.github.io/docs/vscode-cmake-tools/debugging
+// https://www.youtube.com/watch?v=Rfj40xW9q6w&ab_channel=DBersan
+// https://github.com/lu4/startProject
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
