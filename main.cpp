37175cf3c02b0266467f95d917b9ae924efbb932
Denis Yaremov <lu4@Deniss-MBP.technicolor.net>
2022-12-18 22:09:21 +0100

    Added ability to both debug and use cmake

--- main.cpp
+++ main.cpp
@@ -2,15 +2,12 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
 int main(int argc, char const *argv[])
 {
-    /* code */
     std::cout << "OpenCV Hello the world..." << std::endl;
-    Mat image;
-    image = imread("dog.jpg",1);
-    imshow("START PROJECT WITH OPENCV", image);
-    waitKey(0);
+    cv::Mat image;
+    image = cv::imread("dog.jpg",1);
+    cv::imshow("START PROJECT WITH OPENCV", image);
+    cv::waitKey(0);
     return 0;
 }
